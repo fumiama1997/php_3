f63b2f3d73d78578ba26da1d94489357ce99579e
amanofumiya <shiyetianye9@gmail.com>
2024-03-19 14:07:03 +0900

    3/19

---
+++ mvc/tool.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once '../../include/conf/drink_const.php';
+
+require_once '../../include/model/drink.php';
+
+$error = [];
+$drink_data = [];
+$change = '';
+// DB接続
+$link = get_db_connect();
+
+// リクエストメソッド取得
+$request_method = get_request_method();
+if ($request_method === 'POST') {
+
+    $name = get_post_data('name');
+    $price = get_post_data('price');
+    $piece = get_post_data('piace');
+    $file = get_post_data('file');
+    $status = get_post_data('status');
+
+    //名前のチェック
+    if (check_empty($name) === true) {
+        $err_msg[] = '名前を入力してください';
+    }
+
+    //値段のチェック
+    if (check_empty($price) === true) {
+        $err_msg[] = '値段を入力してください';
+    }
+
+    if (check_half_number($price) === false) {
+        $err_msg[] = '値段は半角数字を入力してください';
+    }
+
+    //個数のチェック
+    if (check_empty($piace) === true) {
+        $err_msg[] = '個数を入力してください';
+    }
+
+    if (check_half_number($piece) === false) {
+        $err_msg[] = '個数は半角数字で入力してください';
+    }
+
+    //ファイルのチェック
+    if (check_empty($file) === true) {
+        $err_msg[] = 'ファイルを選択してください';
+    }
+
+    if (check_file($file) === true) {
+        $err_msg[] = 'ファイル形式が異なります。画像ファイルはJPEG又はPNGのみ利用可能です';
+    }
+
+    //ステータスのチェック
+    if (check_status($status) === false) {
+        $err_msg[] = '公開ステータスの値が不正です';
+    }
+
+    if (empty($err_msg)) {
+        mysqli_autocommit($link, false);
+        insert_information_table($name, $price, $date, $status, $file, $link);
+        insert_stock_table($drink_id, $piece, $date, $link);
+        if (count($error) === 0) {
+            // 処理確定
+            mysqli_commit($link);
+            $change = '新規商品追加成功';
+        } else {
+            // 処理取消
+            mysqli_rollback($link);
+        }
+    }
+}
+$drink_data =  get_table_list($link);
+close_db_connect($link);
+
+// HTMLとして表示する文字をHTMLエンティティに変換する
+entity_assoc_array($drink_data);
+
+include_once '../../include/view/tool.php';
