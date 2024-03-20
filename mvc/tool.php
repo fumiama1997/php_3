c6cc4102acb0954ce18adf60a8a91c3f50fee0c9
amanofumiya <shiyetianye9@gmail.com>
2024-03-20 11:51:56 +0900

    3/20

--- mvc/tool.php
+++ mvc/tool.php
@@ -4,70 +4,73 @@ require_once '../../include/conf/drink_const.php';
 
 require_once '../../include/model/drink.php';
 
-$error = [];
+$err_msg = [];
 $drink_data = [];
 $change = '';
 // DB接続
-$link = get_db_connect();
+if ($link = mysqli_connect($host, $user, $passwd, $dbname)) {
+    // リクエストメソッド取得
+    $request_method = get_request_method();
+    if ($request_method === 'POST') {
 
-// リクエストメソッド取得
-$request_method = get_request_method();
-if ($request_method === 'POST') {
+        $name = get_post_data('name');
+        $price = get_post_data('price');
+        $piece = get_post_data('piece');
+        $file = get_post_data('file');
+        $status = get_post_data('status');
 
-    $name = get_post_data('name');
-    $price = get_post_data('price');
-    $piece = get_post_data('piace');
-    $file = get_post_data('file');
-    $status = get_post_data('status');
+        //名前のチェック
+        if (check_empty($name) === true) {
+            $err_msg[] = '名前を入力してください';
+        }
 
-    //名前のチェック
-    if (check_empty($name) === true) {
-        $err_msg[] = '名前を入力してください';
-    }
+        //値段のチェック
+        if (check_empty($price) === true) {
+            $err_msg[] = '値段を入力してください';
+        } else if (check_half_number($price) === false) {
+            $err_msg[] = '値段は半角数字を入力してください';
+        }
 
-    //値段のチェック
-    if (check_empty($price) === true) {
-        $err_msg[] = '値段を入力してください';
-    }
+        //個数のチェック
+        if (check_empty($piece) === true) {
+            $err_msg[] = '個数を入力してください';
+        } else if (check_half_number($piece) === false) {
+            $err_msg[] = '個数は半角数字で入力してください';
+        }
 
-    if (check_half_number($price) === false) {
-        $err_msg[] = '値段は半角数字を入力してください';
-    }
+        //ファイルのチェック
+        if (check_empty($file) === true) {
+            $err_msg[] = 'ファイルを選択してください';
+        } else if (check_file($file) === false) {
+            $err_msg[] = 'ファイル形式が異なります。画像ファイルはJPEG又はPNGのみ利用可能です';
+        }
+        //ステータスのチェック
+        if (check_status($status) === false) {
+            $err_msg[] = '公開ステータスの値が不正です';
+        }
 
-    //個数のチェック
-    if (check_empty($piace) === true) {
-        $err_msg[] = '個数を入力してください';
-    }
+        if (empty($err_msg)) {
 
-    if (check_half_number($piece) === false) {
-        $err_msg[] = '個数は半角数字で入力してください';
-    }
+            $date = date('y:m:d H:i:s');
 
-    //ファイルのチェック
-    if (check_empty($file) === true) {
-        $err_msg[] = 'ファイルを選択してください';
-    }
+            mysqli_autocommit($link, false);
 
-    if (check_file($file) === true) {
-        $err_msg[] = 'ファイル形式が異なります。画像ファイルはJPEG又はPNGのみ利用可能です';
-    }
 
-    //ステータスのチェック
-    if (check_status($status) === false) {
-        $err_msg[] = '公開ステータスの値が不正です';
-    }
+            insert_information_table($name, $price, $date, $status, $file, $link);
 
-    if (empty($err_msg)) {
-        mysqli_autocommit($link, false);
-        insert_information_table($name, $price, $date, $status, $file, $link);
-        insert_stock_table($drink_id, $piece, $date, $link);
-        if (count($error) === 0) {
-            // 処理確定
-            mysqli_commit($link);
-            $change = '新規商品追加成功';
-        } else {
-            // 処理取消
-            mysqli_rollback($link);
+            // 新しく追加した商品のdrink_idを取得
+            $drink_id = mysqli_insert_id($link);
+
+            insert_stock_table($drink_id, $piece, $date, $link);
+
+            if (count($err_msg) === 0) {
+                // 処理確定
+                mysqli_commit($link);
+                $change = '新規商品追加成功';
+            } else {
+                // 処理取消
+                mysqli_rollback($link);
+            }
         }
     }
 }
